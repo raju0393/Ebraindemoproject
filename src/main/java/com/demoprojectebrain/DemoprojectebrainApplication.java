376a92268b467b78d06152ef4d1db02a935c2e6d
DanielSenthilkumar209 <daniels_cse21@velhightech.com>
2025-02-08 12:18:03 +0530

    First commit

---
+++ src/main/java/com/demoprojectebrain/DemoprojectebrainApplication.java
@@ -0,0 +1,13 @@
+package com.demoprojectebrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoprojectebrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoprojectebrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demoprojectebrain/DemoprojectebrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoprojectebrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoprojectebrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
